61d5158e44b32e00b29faeed2c41de7ce85af32b
Christian Vershkov <christian.vershkov@altexsoft.com>
2019-04-16 15:53:14 +0300

    Initial Commit

---
+++ GradientButton/GradientButton.h
@@ -0,0 +1,19 @@
+//
+//  GradientButton.h
+//  GradientButton
+//
+//  Created by Christian Vershkov on 4/16/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradientButton.
+FOUNDATION_EXPORT double GradientButtonVersionNumber;
+
+//! Project version string for GradientButton.
+FOUNDATION_EXPORT const unsigned char GradientButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientButton/PublicHeader.h>
+
+
